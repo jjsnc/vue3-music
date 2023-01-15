6fa1e30459226411fd92e2781753a4f8d9bea242
zhuowenxuan <zhuowenxuan@youfish.com>
2023-01-15 14:19:09 +0800

    列表开发

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -7,6 +7,29 @@
             <slider v-if="sliders.length" :sliders="sliders"></slider>
           </div>
         </div>
+        <div class="recommend-list">
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
+          <ul>
+            <li
+              v-for="item in albums"
+              class="item"
+              :key="item.id"
+              @click="selectItem(item)"
+            >
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.pic">
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{item.title}}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </scroll>
   </div>
