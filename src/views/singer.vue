95ce72514796e751cd298213bb6fb3291924c7d0
zhuowenxuan <zhuowenxuan@youfish.com>
2023-01-15 21:36:37 +0800

    歌手列表

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="search">single</div>
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers" @select="selectSinger"></index-list>
+  </div>
 </template>
 
 <script>
 import { getSingerList } from "@/service/singer";
+import IndexList from "@/components/index-list/index-list";
 export default {
   name: "single",
+  components: {
+    IndexList,
+  },
   data() {
     return {
       singers: [],
@@ -15,6 +21,15 @@ export default {
   async created() {
     const result = await getSingerList();
     this.singers = result.singers;
+  },
+  methods: {
+    selectSinger(singer) {
+      this.selectedSinger = singer;
+      this.cacheSinger(singer);
+      this.$router.push({
+        path: `/singer/${singer.mid}`,
+      });
+    },
   },
 };
 </script>
