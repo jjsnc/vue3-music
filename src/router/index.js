ed95958501a160845b3bd3adcb02767cb5e6c39f
早起喝燕麦 <zhuowangyi@163.com>
2023-01-16 18:53:04 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ const TopList = () =>
   import("@/views/top-list" /* webpackChunkName: "top-list" */);
 const Search = () => import("@/views/search" /* webpackChunkName: "search" */);
 
+const SingerDetail = () => import('@/views/singer-detail'/* webpackChunkName: "singer-detail" */)
+
 const routes = [
   {
     path: "/",
@@ -18,6 +20,12 @@ const routes = [
   {
     path: "/singer",
     component: Singer,
+    children: [
+      {
+        path: ':id',
+        component: SingerDetail
+      }
+    ]
   },
   {
     path: "/top-list",
