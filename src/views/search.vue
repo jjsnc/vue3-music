9db5a99d4eac2815c7c9e511e2fbe5320a1c9440
早起喝燕麦 <zhuowangyi@163.com>
2023-02-23 14:01:20 +0800

    input

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="search">search</div>
+  <div class="search-input-wrapper">
+    <search-input v-model="query"></search-input>
+  </div>
 </template>
 
 <script>
+import SearchInput from "@/components/search/search-input";
+import { ref } from "vue";
 export default {
   name: "search",
+  components: {
+    SearchInput,
+  },
+  setup() {
+    const query = ref("");
+
+    return {
+      query,
+    };
+  },
 };
 </script>
 
